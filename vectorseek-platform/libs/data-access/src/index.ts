5742574372065831eebd8c4253e88ca2c8395b65
Vinicius Koiti <viniciusnakahara@gmail.com>
2025-11-21 11:24:34 -0300

    adicionando informações

--- vectorseek-platform/libs/data-access/src/index.ts
+++ vectorseek-platform/libs/data-access/src/index.ts
@@ -12,7 +12,6 @@ export * from './lib/documents/documents.service';
 
 
 export * from './lib/workspaces/workspaces.api';
-export * from './lib/workspaces/workspaces.models';
 export * from './lib/workspaces/workspaces.service';
 export * from './lib/documents/documents.service';
 
@@ -21,7 +20,6 @@ export * from './lib/generation/generation.models';
 export * from './lib/generation/generation.service';
 
 export * from './lib/workspaces/workspaces.api';
-export * from './lib/workspaces/workspaces.models';
 export * from './lib/workspaces/workspaces.service';
 export * from './lib/generation/generation-history.service';
 
